8b245d6c5819c08b8a4c0ea62406f392c77ab120
Dmytro Kulyk <13dimon1313@gmail.com>
2022-07-06 02:02:13 +0300

    connect github api & create card

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,45 @@
-fetch('https://api.github.com/search/repositories?q=')
-    .then((response) => {
-        // console.log(response.json())
-        console.log(response);
-    })
-    // .then((data) => {
-        // console.log(data);
-    // });
+let searchField = document.getElementById('searchInput');
+let searchBtn = document.getElementById('searchBtn');
+let cardListEl = document.getElementById('cardList');
+let searchValue;
+let DATA;
+
+
+async function getData(login) {
+    return await fetch(`https://api.github.com/users/${login}`)
+        .then(res => res.json())
+        .then(res => {
+            DATA = res;
+        })
+}
+
+searchBtn.addEventListener('click', e => {
+    if (searchField.value !== '') {
+        searchValue = searchField.value.trim();
+        getData(searchValue);
+        renderCard(DATA, cardListEl)
+        console.log(DATA.name)
+        searchField.value = '';
+    }
+})
+
+
+function renderCard(data, element) {
+    let html = '';
+    html += createCard(data)
+    element.insertAdjacentHTML('beforeEnd', html)
+}
+
+function createCard(repo_data) {
+    return `
+        <div class="card col-6" style="width: 18rem;">
+            <img src="/" class="card-img-top" alt="User image">
+            <div class="card-body">
+                <h5 class="card-title">l,as</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    `
+}
+
