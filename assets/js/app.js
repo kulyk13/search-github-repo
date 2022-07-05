9ec1f4c8dc6c8fb27c98f3220d4360f6ae138d29
Dmytro Kulyk <13dimon1313@gmail.com>
2022-07-05 11:44:34 +0300

    create project

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+fetch('https://api.github.com/search/repositories?q=')
+    .then((response) => {
+        // console.log(response.json())
+        console.log(response);
+    })
+    // .then((data) => {
+        // console.log(data);
+    // });
